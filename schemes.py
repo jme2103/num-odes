49462a7215d5340044fde8082bc0de75dd9134d6
jme2103 <43256224+jme2103@users.noreply.github.com>
2019-09-10 17:04:37 -0500

    Update schemes.py
    added plotting of the energy against time next to the plot of the trajectory

--- schemes.py
+++ schemes.py
@@ -15,7 +15,7 @@ def eulerstep(phi, z, deltat):
 
 z0 = np.array([2,0])
 deltat = 0.0001
-numSteps = 1000000
+numSteps = 500000
 
 # potential for Morse oscillator
 # phi = lambda q : (1 - np.exp(-q))**2
@@ -29,7 +29,22 @@ soln[0] = z0
 for i in range(1,numSteps):
 	soln[i] = eulerstep(phi, soln[i-1], deltat)
 
+plt.subplot(121)
+plt.xlabel("position")
+plt.ylabel("velocity")
+plt.title("a single trajectory")
 plt.plot(np.transpose(soln)[0], np.transpose(soln)[1])
+
+plt.subplot(122)
+plt.xlabel("time")
+plt.ylabel("energy")
+plt.title("a single trajectory")
+plt.plot(np.transpose(soln)[0], np.transpose(soln)[1])
+
+
+
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=1, hspace=0.2)
 plt.show()
 
 #print(np.transpose(soln)[0])
