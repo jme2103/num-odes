5268db1d2725c21fa6b24a10c0891f5e469e28d9
jme2103 <43256224+jme2103@users.noreply.github.com>
2019-09-09 22:15:53 -0500

    Update schemes.py
    Added the Euler step function and worked out basic plotting of solutions in state space.

--- schemes.py
+++ schemes.py
@@ -1,1 +1,36 @@
-# Implementing several standard numerical integration schemes for autonomous ODEs of the form x' = f(x)
+# This script implements several standard numerical integration schemes first-order systems of the form
+# 	z' = f(z)
+# For simplicity, this only solves conservative systems with a potential, q'' = -phi'(q).
+# As a first-order system, this is equivalent to q' = v, v' = -phi'(q). We let z = [q, v] be the
+# state vector, where q is the position and v is the velocity.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def eulerstep(phi, z, deltat):
+	nextStep = np.zeros(2)
+	nextStep[0] = z[0] + deltat*z[1]
+	nextStep[1] = z[1] + deltat*phi(z[0])
+	return nextStep
+
+z0 = np.array([2,0])
+deltat = 0.0001
+numSteps = 1000000
+
+# potential for Morse oscillator
+# phi = lambda q : (1 - np.exp(-q))**2
+
+# potential for Lennard-Jones oscillator
+phi = lambda q : q**(-12) - 2*q**(-6)
+
+soln = np.zeros((numSteps,2))
+soln[0] = z0
+
+for i in range(1,numSteps):
+	soln[i] = eulerstep(phi, soln[i-1], deltat)
+
+plt.plot(np.transpose(soln)[0], np.transpose(soln)[1])
+plt.show()
+
+#print(np.transpose(soln)[0])
+#print(np.transpose(soln)[1])
